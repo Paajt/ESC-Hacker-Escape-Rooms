1acabca15941ec7e33e387dbcca1541a4b15fb5c
Emilyempa <emilypettersson@hotmail.com>
2024-11-15 15:45:03 +0100

    function to sort API challenges by ranking and make array with 3 top ranking

--- main.js
+++ main.js
@@ -17,3 +17,23 @@ function closeMobileMenu() {
 
 openBtn.addEventListener('click', openMobileMenu);
 closeBtn.addEventListener('click', closeMobileMenu);
+
+
+async function highestRanking() {
+    try{
+        const response = await fetch("https://lernia-sjj-assignments.vercel.app/api/challenges");
+        const data = await response.json();
+        data.challenges.sort(function(a, b){return a.rating - b.rating});
+        let start = data.challenges.length -3;
+        data.challenges.splice(0, start);
+        console.log(data.challenges);
+        
+       
+    }catch (error){
+        console.error("error", error);
+        }
+        
+    };
+    
+          
+    highestRanking()
