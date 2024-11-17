0e9f31e35613be664d6161ab01e49bb56d23dea0
Emilyempa <emilypettersson@hotmail.com>
2024-11-17 19:44:12 +0100

    update all three cards with most popular cards api

--- main.js
+++ main.js
@@ -26,7 +26,17 @@ async function highestRanking() {
         data.challenges.sort(function(a, b){return b.rating - a.rating});
         const topThree =data.challenges.slice(0, 3);
         console.log(topThree);
-        
+        const cards = document.querySelectorAll(".roomIndividual");
+      topThree.forEach((challenge, index) => {
+        if (cards[index]) {
+          const card = cards[index];
+          card.querySelector(".hackerImg").src = challenge.image;
+          card.querySelector(".titleRoom").textContent = challenge.title;
+          card.querySelector(".typeRoom").textContent = challenge.type;
+          card.querySelector(".descriptionRoom").textContent = challenge.description;  
+          card.querySelector(".participantsRoom").textContent = `${challenge.minParticipants}-${challenge.maxParticipants} participants`;
+        }
+      });
        
     }catch (error){
         console.error("error", error);
