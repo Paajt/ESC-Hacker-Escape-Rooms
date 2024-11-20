892d0339c5c3f9f64c173455b9e88be2ac9f9299
itsJoel59 <joel.j.jansson@gmail.com>
2024-11-20 03:33:20 +0100

    Updated the card builder with star-rating, as well as styling components

--- load-challenges.js
+++ load-challenges.js
@@ -11,14 +11,13 @@ async function cardAPI() {
 
     function createCards (challenges) {
         const template = document.querySelector(".card-template");
-        const container = document.querySelector(".card-container");
+        const container = document.querySelector(".roomContainer");
 
         challenges.forEach(challenge => {
             const clone = template.content.cloneNode(true);
-            clone.querySelector(".img").src = challenge.image;
-            clone.querySelector(".title").textContent = challenge.title;
-            clone.querySelector(".onsite-online").textContent = challenge.type;
-            clone.querySelector(".rating")
+            clone.querySelector(".hackerImg").src = challenge.image;
+            clone.querySelector(".title").textContent = challenge.title + ' (' + challenge.type + ')';
+            createStars(challenge.rating, clone.querySelector(".imgContainer"));
             clone.querySelector(".ppl").textContent = `${challenge.minParticipants}-${challenge.maxParticipants} participants`;
             clone.querySelector(".describe").textContent = challenge.description;
             container.appendChild(clone);
@@ -26,3 +25,51 @@ async function cardAPI() {
     
 
     cardAPI();
+
+        // Star-rating builder
+    function createStars(rating, container) {
+
+        let filledStars = 0, halfStar = 0;
+    
+        if (rating % 2 == 0.5 || rating % 2 == 1.5){
+    
+            filledStars = Math.floor(rating);
+            halfStar = 1;
+        }
+    
+        else {
+    
+            filledStars = rating;
+        }
+    
+    
+        for (i = 0; i < 5; i++){
+    
+            if (filledStars >= i + 1){
+                const star = document.createElement('img');
+                star.setAttribute('src', 'img/StarFull.png')
+                container.appendChild(star);
+            }
+            else if (halfStar == 1){
+                const star = document.createElement('div');
+                star.setAttribute('id','halfStar');
+                const starEmpty = document.createElement('img');
+                starEmpty.setAttribute('src', 'img/StarEmpty.png');
+                starEmpty.setAttribute('class', 'star');
+                starEmpty.setAttribute('id', 'empty');
+                star.appendChild(starEmpty);
+                const starFilled = document.createElement('img');
+                starFilled.setAttribute('class', 'star halfFilled');
+                starFilled.setAttribute('src', 'img/StarFull.png');
+                star.appendChild(starFilled);
+                container.appendChild(star);
+                halfStar = 0;
+            }
+            else {
+                const star = document.createElement('img');
+                star.setAttribute('src', 'img/StarEmpty.png');
+                container.appendChild(star);
+            }
+        }
+    
+    }
