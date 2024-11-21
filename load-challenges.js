02940f4a539da9036feee8e28187fdbffc301283
itsJoel59 <joel.j.jansson@gmail.com>
2024-11-21 14:18:36 +0100

    Fixed template class names

--- load-challenges.js
+++ load-challenges.js
@@ -11,17 +11,25 @@ async function cardAPI() {
 
     function createCards (challenges) {
         const template = document.querySelector(".card-template");
-        const container = document.querySelector(".card-container");
+        const container = document.querySelector(".roomContainer");
 
         challenges.forEach(challenge => {
             const clone = template.content.cloneNode(true);
-            clone.querySelector(".img").src = challenge.image;
-            clone.querySelector(".title").textContent = challenge.title;
-            createStars(challenge.rating, clone.querySelector(".stars-container"));
-            clone.querySelector(".onsite-online").textContent = challenge.type;
-            clone.querySelector(".ppl").textContent = `${challenge.minParticipants}-${challenge.maxParticipants} participants`;
-            clone.querySelector(".describe").textContent = challenge.description;
-            const cardButton = clone.querySelector("button");
+            clone.querySelector(".hackerImg").src = challenge.image;
+            clone.querySelector(".titleRoom").textContent = challenge.title;
+            createStars(challenge.rating, clone.querySelector(".imgContainer"));
+            clone.querySelector(".participantsRoom").textContent = `${challenge.minParticipants}-${challenge.maxParticipants} participants`;
+            clone.querySelector(".descriptionRoom").textContent = challenge.description;
+            const cardButton = clone.querySelector("a");
+            if (challenge.type.includes("onsite")) {
+                clone.querySelector(".typeRoom").textContent = `(${challenge.type})`;
+                clone.querySelector(".iconRoom").src = "./img/home.png";
+                clone.querySelector(".bookBtn").textContent = "Book this room";
+              }else{
+                  clone.querySelector(".iconRoom").src = "./img/laptop.png";
+                  clone.querySelector(".bookBtn").textContent = "Take challenge online";
+                  clone.querySelector(".typeRoom").textContent = "";
+                }
 
             cardButton.addEventListener("click", () => {
                 challengePeople = [];
