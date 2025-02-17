cf76bca02792d4f70bb0bb5c5fa00ffa9f0feb02
Alfred <alfred-brannarer@hotmail.com>
2025-02-17 15:08:28 +0100

    Added alt to star images and added a frontpage link on logo

--- createStars.js
+++ createStars.js
@@ -19,7 +19,8 @@ function createStars(rating, container) {
 
         if (filledStars >= i + 1) {
             const star = document.createElement('img');
-            star.setAttribute('src', 'img/StarFull.png')
+            star.setAttribute('src', 'img/StarFull.png');
+            star.setAttribute('alt', 'Full star');
             container.appendChild(star);
         }
         else if (halfStar == 1) {
@@ -29,10 +30,12 @@ function createStars(rating, container) {
             starEmpty.setAttribute('src', 'img/StarEmpty.png');
             starEmpty.setAttribute('class', 'star');
             starEmpty.setAttribute('id', 'empty');
+            starEmpty.setAttribute('alt', 'Empty star');
             star.appendChild(starEmpty);
             const starFilled = document.createElement('img');
             starFilled.setAttribute('class', 'star halfFilled');
             starFilled.setAttribute('src', 'img/StarFull.png');
+            starFilled.setAttribute('alt', 'Half-filled star');
             star.appendChild(starFilled);
             container.appendChild(star);
             halfStar = 0;
