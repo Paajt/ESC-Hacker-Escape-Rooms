d843cd7b4a79fe602c04d1861197236cf35694ed
Alfred <alfred-brannarer@hotmail.com>
2025-02-24 21:00:42 +0100

    Updated cypress config

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl: 'https://paajt.github.io/ESC-Hacker-Escape-Rooms/',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
